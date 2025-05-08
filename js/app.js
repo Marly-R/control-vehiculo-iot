f4ed92f20f425cb4ad05196fc45c67da1784af89
MarlyR <l22201111@pachuca.tecnm.mx>
2025-05-07 21:40:10 -0600

    segunda modificacion

---
+++ js/app.js
@@ -0,0 +1,29 @@
+function sendCommand(command) {
+    const data = {
+        name: "Marly Galarza",
+        ip: "192.123.23.43",
+        status: command
+    };
+
+    fetch('http://3.238.156.64/api/devices', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error en la solicitud');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Respuesta del servidor:', data);
+        document.getElementById('movementStatus').textContent = command;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('movementStatus').textContent = "Error al enviar orden";
+    });
+}
